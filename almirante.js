58f4df611baa804e55577f4ed45fc0e779504d54
edgar.garcia <gader32@gmail.com>
2016-01-13 21:43:42 -0600

    adding correct dates

--- almirante.js
+++ almirante.js
@@ -39,9 +39,9 @@ var TIMEOUT_SECONDS = 180;
 var string2array = function(urls){
   var url = urls.split("\n");
   for(var i=0;i< url.length;i++){
-  	//slack sends urls with < and > they should be removed
-  	url[i] = url[i].replace(/</g, '');
-  	url[i] = url[i].replace(/>/g, '');
+    //slack sends urls with < and > they should be removed
+    url[i] = url[i].replace(/</g, '');
+    url[i] = url[i].replace(/>/g, '');
   }
   return url;
 };
@@ -54,10 +54,10 @@ var string2array = function(urls){
  */
 var startWaiting = function(channel){
   waitingTimeout = setTimeout(function(){
-  	if(queue.length>0){
-  		channel.send('@'+queue[0].user+' has been kicked after 3 minutes of inactivity.');
-  		queue.shift();
-  	}
+    if(queue.length>0){
+        channel.send('@'+queue[0].user+' has been kicked after 3 minutes of inactivity.');
+        queue.shift();
+    }
   },TIMEOUT_SECONDS*1000);
 };
 
@@ -67,7 +67,7 @@ var startWaiting = function(channel){
  * @name stopWaiting
  */
 var stopWaiting = function(){
-	clearTimeout(waitingTimeout);
+    clearTimeout(waitingTimeout);
 };
 
 /**
@@ -80,19 +80,19 @@ var stopWaiting = function(){
  * @param {string} message - message sent to slackbot
  */
 commands.lista = function(user,channel,message){
-	var urls = string2array(message);
-	if(urls.length<1){
-		return;
-	}
-	var count=0;
-	var interval = setInterval(function() {
-		if(count< urls.length){
-			channel.send(urls[count]);	
-			count++;
-		} else{
-			clearInterval(interval);
-		}
-	}, 1500);
+    var urls = string2array(message);
+    if(urls.length<1){
+        return;
+    }
+    var count=0;
+    var interval = setInterval(function() {
+        if(count< urls.length){
+            channel.send(urls[count]);  
+            count++;
+        } else{
+            clearInterval(interval);
+        }
+    }, 1500);
 };
 
 /**
@@ -105,23 +105,23 @@ commands.lista = function(user,channel,message){
  * @param {string} message - message sent to slackbot
  */
 commands.queue = function(user,channel,message){
-	if(queue.length<1){
-		channel.send('Queue is empty');
-		return;
-	}
-	var count=0;
-	var interval = setInterval(function() {
-		if(count< queue.length){
-			channel.send(
-				queue[count].user+'\n'+
-				'Requested time:'+queue[count].reqeuested_at+'\n'+
-				'Start time:'+queue[count].start_at
-				);	
-			count++;
-		} else{
-			clearInterval(interval);
-		}
-	}, 500);
+    if(queue.length<1){
+        channel.send('Queue is empty');
+        return;
+    }
+    var count=0;
+    var interval = setInterval(function() {
+        if(count< queue.length){
+            channel.send(
+                queue[count].user+'\n'+
+                'Requested time:'+queue[count].reqeuested_at+'\n'+
+                'Start time:'+queue[count].start_at
+                );  
+            count++;
+        } else{
+            clearInterval(interval);
+        }
+    }, 500);
 };
 
 /**
@@ -134,30 +134,30 @@ commands.queue = function(user,channel,message){
  * @param {string} message - message sent to slackbot
  */
 commands.ficha = function(user,channel,message){
-	if(queue.length<1){
-		//if no one in the queue, assing turn immediately 
-		queue.push({
-			user: String(user.name),
-			reqeuested_at: new Date(),
-			start_at: new Date()
-		});
-		channel.send('Queue is empty you are free to update in the next 10 minutes, there\'s no need to call /voy');
-	}else{
-		//check if doesn't exist in queue already
-		for (var i=0;i<queue.length;i++) {
-			console.log(queue[i].user);
-		  if (queue[i].user === String(user.name)){
-		    channel.send(user.name+' is in the queue already, call /queue to view the queue');
-		    return;
-		  }
-		}
-		queue.push({
-			user: user.name,
-			reqeuested_at: new Date(),
-			start_at: undefined
-		});
-		channel.send(user.name+' has been added to the queue');
-	}
+    if(queue.length<1){
+        //if no one in the queue, assing turn immediately 
+        queue.push({
+            user: String(user.name),
+            reqeuested_at: new Date(),
+            start_at: new Date()
+        });
+        channel.send('Queue is empty you are free to update in the next 10 minutes, there\'s no need to call /voy');
+    }else{
+        //check if doesn't exist in queue already
+        for (var i=0;i<queue.length;i++) {
+            console.log(queue[i].user);
+          if (queue[i].user === String(user.name)){
+            channel.send(user.name+' is in the queue already, call /queue to view the queue');
+            return;
+          }
+        }
+        queue.push({
+            user: user.name,
+            reqeuested_at: new Date(),
+            start_at: undefined
+        });
+        channel.send(user.name+' has been added to the queue');
+    }
 };
 
 /**
@@ -170,37 +170,37 @@ commands.ficha = function(user,channel,message){
  * @param {string} message - message sent to slackbot
  */
 commands.next = function(user,channel,message){
-	//return if queue is empty
-	if(queue.length<1){
-		channel.send('Queue is empty');
-		return;
-	}
-	var queueUser = String(queue[0].user);
-	if(queueUser === user.name){
-		//user never called /voy
-		if(!queue[0].start_at){
-			channel.send('Please call /voy before calling /next');
-			return;
-		}
-		//check if user took more than 10 minutes to update
-		var now = new Date();
-		var timeDiff = (now.getTime() - queue[0].start_at.getTime())/1000;
-		if(timeDiff>600){
-			channel.send('Thanks for the donuts '+queueUser+', time updating:'+timeDiff/60);
-		}
-		queue.shift();
-		if(queue.length<1){
-			channel.send('Thanks for updating, queue is empty');
-		}else{
-			queueUser = String(queue[0].user);
-			channel.send('@'+queueUser+' is next in the queue and has 3 minutes to start updating.');	
-			//start waiting 3 minutes for user to get its turn.
-			startWaiting(channel);
-		}
-	}else{
-		//someone else tried to update
-		channel.send(queueUser+' is updating please wait.');
-	}
+    //return if queue is empty
+    if(queue.length<1){
+        channel.send('Queue is empty');
+        return;
+    }
+    var queueUser = String(queue[0].user);
+    if(queueUser === user.name){
+        //user never called /voy
+        if(!queue[0].start_at){
+            channel.send('Please call /voy before calling /next');
+            return;
+        }
+        //check if user took more than 10 minutes to update
+        var now = new Date();
+        var timeDiff = (now.getTime() - queue[0].start_at.getTime())/1000;
+        if(timeDiff>600){
+            channel.send('Thanks for the donuts '+queueUser+', time updating:'+timeDiff/60);
+        }
+        queue.shift();
+        if(queue.length<1){
+            channel.send('Thanks for updating, queue is empty');
+        }else{
+            queueUser = String(queue[0].user);
+            channel.send('@'+queueUser+' is next in the queue and has 3 minutes to start updating.');   
+            //start waiting 3 minutes for user to get its turn.
+            startWaiting(channel);
+        }
+    }else{
+        //someone else tried to update
+        channel.send(queueUser+' is updating please wait.');
+    }
 };
 
 /**
@@ -213,20 +213,20 @@ commands.next = function(user,channel,message){
  * @param {string} message - message sent to slackbot
  */
 commands.kick = function(user,channel,message){
-	//only admin users should perform this action
-	if(admins.indexOf(String(user.name)) !== -1){
-		var queueUser = String(queue[0].user);
-		channel.send(queueUser+' has been removed for inactivity');
-		queue.shift();
-		if(queue.length<1){
-			channel.send('Queue is empty');
-		}else{
-			queueUser = String(queue[0].user);
-			channel.send(queueUser+' is next in the queue');	
-		}
-	}else{
-		channel.send('You are not authorized to perform this action');
-	}
+    //only admin users should perform this action
+    if(admins.indexOf(String(user.name)) !== -1){
+        var queueUser = String(queue[0].user);
+        channel.send(queueUser+' has been removed for inactivity');
+        queue.shift();
+        if(queue.length<1){
+            channel.send('Queue is empty');
+        }else{
+            queueUser = String(queue[0].user);
+            channel.send(queueUser+' is next in the queue');    
+        }
+    }else{
+        channel.send('You are not authorized to perform this action');
+    }
 };
 
 /**
@@ -239,23 +239,23 @@ commands.kick = function(user,channel,message){
  * @param {string} message - message sent to slackbot
  */
 commands.voy = function(user,channel,message){
-	//return if queue is empty
-	if(queue.length<1){
-		channel.send('Queue is empty');
-		return;
-	}
-	var queueUser = String(queue[0].user);
-	if(queueUser === user.name){
-		if(queue[0].start_at){
-			channel.send('You requested a turn already, your time started at:'+queue[0].start_at);	
-			return;
-		}
-		stopWaiting();
-		channel.send('You have 10 minutes to update');
-		queue[0].start_at = new Date();
-	}else{
-		channel.send(queueUser+' is updating');
-	}
+    //return if queue is empty
+    if(queue.length<1){
+        channel.send('Queue is empty');
+        return;
+    }
+    var queueUser = String(queue[0].user);
+    if(queueUser === user.name){
+        if(queue[0].start_at){
+            channel.send('You requested a turn already, your time started at:'+queue[0].start_at);  
+            return;
+        }
+        stopWaiting();
+        channel.send('You have 10 minutes to update');
+        queue[0].start_at = new Date();
+    }else{
+        channel.send(queueUser+' is updating');
+    }
 };
 
 /**
@@ -268,36 +268,38 @@ commands.voy = function(user,channel,message){
  * @param {string} message - message sent to slackbot
  */
 commands.parking = function(user,channel,message){
-	console.log('using parking dude');
-	var date = moment().add(-1,'days');
-	var month = date.format('MM');
-	var day   = date.format('DD');
-	var year  = date.format('YYYY');
-	var fromDate = moment(year+"-"+month+"-"+day+"T00:00:00").utc().toISOString();
-	var toDate = moment(year+"-"+month+"-"+day+"T23:59:00").utc().toISOString();
-	console.log(fromDate);
-	console.log(toDate);
-	var url = 'https://api.parse.com/1/classes/Events?include=user&order=startsAt&where={"startsAt":{"$gte":{"__type":"Date","iso":"'+fromDate+'"}},"endsAt":{"$lte":{"__type":"Date","iso":"'+toDate+'"}}}';
-	console.log(url);
-	unirest.get(url)
-	.headers({
-		'X-Parse-Application-Id': 'HMgEYiz7FYsYo4yymyJzcjkIzBuxo5SZDfKKBAoJ',
-		'X-Parse-REST-API-Key': 'N9aoa2aWcwYWmNjKNc0DuTHALh28YHzVf2C8S4QW',
-		'Content-Type':'application/json'
-	})
-	.end(function(response){
-		console.log('Success call and getting values');
-		var results = response.body.results;
-		console.log(results);
-		for(var i=0;i<results.length;i++){
-			//filter results for today only
+    var today = moment().utc().add(-6,'hours');
+    today = today.add(1,'day');
+    var month = today.format('MM');
+    var day   = today.format('DD');
+    var year  = today.format('YYYY');
+    var fromDate = moment(year+"-"+month+"-"+day+"T00:01:00").utc().toISOString();
+    var nextDay = today.add(1,'day');
+    month = today.format('MM');
+    day   = today.format('DD');
+    year  = today.format('YYYY');
+    var toDate = moment(year+"-"+month+"-"+day+"T00:01:00").utc().toISOString();
+    var url = 'https://api.parse.com/1/classes/Events?include=user&order=startsAt&where={"startsAt":{"$gte":{"__type":"Date","iso":"'+fromDate+'"}},"endsAt":{"$lte":{"__type":"Date","iso":"'+toDate+'"}}}';
+    console.log(url);
+    unirest.get(url)
+    .headers({
+        'X-Parse-Application-Id': 'HMgEYiz7FYsYo4yymyJzcjkIzBuxo5SZDfKKBAoJ',
+        'X-Parse-REST-API-Key': 'N9aoa2aWcwYWmNjKNc0DuTHALh28YHzVf2C8S4QW',
+        'Content-Type':'application/json'
+    })
+    .end(function(response){
+        console.log('Success call and getting values');
+        var results = response.body.results;
+        console.log(results);
+        for(var i=0;i<results.length;i++){
+            //filter results for today only
 
-			var currentUser = results[i].user;
-			var name = currentUser.first_name+' '+currentUser.last_name;
-			console.log('USer'+name);
-			channel.send(name);
-		}
-	});
+            var currentUser = results[i].user;
+            var name = currentUser.first_name+' '+currentUser.last_name;
+            console.log('USer'+name);
+            channel.send(name);
+        }
+    });
 };
 
 /**
